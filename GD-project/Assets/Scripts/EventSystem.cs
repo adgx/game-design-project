c691648e5f0eec167f3a846fcffffc10d20b70b6
adgx <antonino.d.gregorio@gmail.com>
2025-04-09 12:17:46 +0200

    Shader to make disappear and appear an object done

---
+++ GD-project/Assets/Scripts/EventSystem.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class EventSystem : MonoBehaviour
+{
+    private static EventSystem _instance;
+    public static EventSystem Instance => _instance;
+    public event EventHandler EventToDissolve;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        LunchDissolveEvent();
+    }
+
+    void LunchDissolveEvent()
+    {
+        //I'm using Space key for Testing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (EventToDissolve != null)
+            {
+                Debug.Log("Lunch the event");
+                EventToDissolve(this, EventArgs.Empty);
+            }
+        }
+    }
+}
