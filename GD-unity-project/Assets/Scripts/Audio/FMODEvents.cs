39be2ae33df7b80c08199baef7121ecb3a823eda
Federico La Macchia s333959 <s333959@studenti.polito.it>
2025-06-20 17:37:29 +0200

    Modified Drake's SFXs

--- GD-unity-project/Assets/Scripts/Audio/FMODEvents.cs
+++ GD-unity-project/Assets/Scripts/Audio/FMODEvents.cs
@@ -45,8 +45,7 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference maynardFootstep2 { get; private set; }
     
     [field: Header("Drake SFX")]
-    [field: SerializeField] public EventReference drakeCloseAttack1Roar { get; private set; }
-    [field: SerializeField] public EventReference drakeCloseAttack1Bite { get; private set; }
+    [field: SerializeField] public EventReference drakeCloseAttack1 { get; private set; }
     [field: SerializeField] public EventReference drakeCloseAttack2 { get; private set; }
     [field: SerializeField] public EventReference drakeDefense { get; private set; }
     [field: SerializeField] public EventReference drakeDieHit { get; private set; }
