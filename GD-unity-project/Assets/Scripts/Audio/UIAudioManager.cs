1f03c147470950fa085fb2bce4e613ba62ff558c
adgx <antonino.d.gregorio@gmail.com>
2025-07-29 23:38:07 +0200

    try to fix player+map

--- GD-unity-project/Assets/Scripts/Audio/UIAudioManager.cs
+++ GD-unity-project/Assets/Scripts/Audio/UIAudioManager.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using UnityEngine;
 
 namespace Audio
@@ -6,42 +7,42 @@ namespace Audio
     {
         public void PlayOpenSound()
         {   
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.PauseMenuOpen, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.PauseMenuOpen, transform.position);
         }
         
         public void PlayCloseSound()
         {       
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.PauseMenuClose, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.PauseMenuClose, transform.position);
         }
         
         public void PlayPositiveSelectionSound()
         {
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.PauseMenuPositiveSelection, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.PauseMenuPositiveSelection, transform.position);
         }
         
         public void PlayNegativeSelectionSound()
         {       
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.PauseMenuNegativeSelection, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.PauseMenuNegativeSelection, transform.position);
         }
 
         public void PlayMasterVolumeControlSliderSound()
         {
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.MasterVolumeControlSlider, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.MasterVolumeControlSlider, transform.position);
         }
         
         public void PlayMusicVolumeControlSliderSound()
         {
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.MusicVolumeControlSlider, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.MusicVolumeControlSlider, transform.position);
         }
         
         public void PlayAmbienceVolumeControlSliderSound()
         {
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.AmbienceVolumeControlSlider, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.AmbienceVolumeControlSlider, transform.position);
         }
         
         public void PlaySfxVolumeControlSliderSound()
         {
-            GamePlayAudioManager.instance.PlayOneShot(FMODEvents.Instance.SfxVolumeControlSlider, transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.SfxVolumeControlSlider, transform.position);
         }
     }
 }
