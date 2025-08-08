cd3f1bc2228f8f0584b5834dc7ec02f3264c0def
Federico La Macchia s333959 <s333959@studenti.polito.it>
2025-08-08 15:23:21 +0200

    Added a <<grace time>> for Incognito (he does not attack immediately when Rick enters the room

--- GD-unity-project/Assets/Scripts/Enemy/EnemyFSM/EnemyIncognito.cs
+++ GD-unity-project/Assets/Scripts/Enemy/EnemyFSM/EnemyIncognito.cs
@@ -57,6 +57,10 @@ public class Incognito : MonoBehaviour, IEnemy
     private State _deathS;
 
     private EnemyManager enemyManager;
+    
+    [SerializeField] private float _gracePeriod = 5f; // Time in seconds before he can attack the player
+    private float _graceTimer;
+    private bool _graceActive = true;
 
     // Audio management
     private IncognitoEvents _events;
@@ -98,6 +102,9 @@ public class Incognito : MonoBehaviour, IEnemy
 
     void Start()
     {
+        _graceTimer = _gracePeriod;
+        _graceActive = true;
+        
         if (_debug)
         {
             _agent.speed = 5f;
@@ -131,22 +138,24 @@ public class Incognito : MonoBehaviour, IEnemy
         _reactFromFrontS = new IncognitoReactFromFrontState("Hit", this);
         _deathS = new IncognitoDeathState("Death", this);
 
+        bool canAttack = !_graceActive && _playerInSightRange && _playerInAttackRange;
+        
         //Transition
         //idle
         _stateMachine.AddTransition(idleS, patrolS, () => !_playerInSightRange && _waitCurTime >= _timeIdle);
-        _stateMachine.AddTransition(idleS, chaseS, () => _playerInSightRange && !_playerInAttackRange);
-        _stateMachine.AddTransition(idleS, wonderS, () => _playerInSightRange && _playerInAttackRange);
+        _stateMachine.AddTransition(idleS, chaseS, () => !_graceActive && _playerInSightRange && !_playerInAttackRange);
+        _stateMachine.AddTransition(idleS, wonderS, () => !_graceActive && _playerInSightRange && _playerInAttackRange);
         //patrol
-        _stateMachine.AddTransition(patrolS, chaseS, () => _playerInSightRange && !_playerInAttackRange);
-        _stateMachine.AddTransition(patrolS, wonderS, () => _playerInSightRange && _playerInAttackRange);
+        _stateMachine.AddTransition(patrolS, chaseS, () => !_graceActive && _playerInSightRange && !_playerInAttackRange);
+        _stateMachine.AddTransition(patrolS, wonderS, () => !_graceActive && _playerInSightRange && _playerInAttackRange);
         //chase
         _stateMachine.AddTransition(chaseS, patrolS, () => !_playerInSightRange && !_playerInAttackRange);
-        _stateMachine.AddTransition(chaseS, wonderS, () => _playerInSightRange && _playerInAttackRange);
+        _stateMachine.AddTransition(chaseS, wonderS, () => !_graceActive && _playerInSightRange && _playerInAttackRange);
         //wonderS
         _stateMachine.AddTransition(wonderS, waitS, () => _alreadyAttacked);
-        _stateMachine.AddTransition(wonderS, shortSpitAttackS, () => !_alreadyAttacked && _playerInSightRange && _playerInAttackRange && shortDistanceSpit);
-		_stateMachine.AddTransition(wonderS, longSpitAttackS, () => !_alreadyAttacked && _playerInSightRange && _playerInAttackRange && !shortDistanceSpit);
-		_stateMachine.AddTransition(wonderS, chaseS, () => _playerInSightRange && !_playerInAttackRange);
+        _stateMachine.AddTransition(wonderS, shortSpitAttackS, () => !_graceActive && !_alreadyAttacked && _playerInSightRange && _playerInAttackRange && shortDistanceSpit);
+		_stateMachine.AddTransition(wonderS, longSpitAttackS, () => !_graceActive && !_alreadyAttacked && _playerInSightRange && _playerInAttackRange && !shortDistanceSpit);
+		_stateMachine.AddTransition(wonderS, chaseS, () => !_graceActive && _playerInSightRange && !_playerInAttackRange);
         _stateMachine.AddTransition(wonderS, patrolS, () => !_playerInSightRange && !_playerInAttackRange);
         //shortSpitAttack
         _stateMachine.AddTransition(shortSpitAttackS, wonderS, () => anim.EndShortSpit);
@@ -155,35 +164,58 @@ public class Incognito : MonoBehaviour, IEnemy
 		_stateMachine.AddTransition(longSpitAttackS, wonderS, () => anim.EndLongSpit);
 		_stateMachine.AddTransition(longSpitAttackS, waitS, () => _alreadyAttacked);
 		//wait
-		_stateMachine.AddTransition(waitS, wonderS, () => !_alreadyAttacked);
+		_stateMachine.AddTransition(waitS, wonderS, () => !_graceActive && !_alreadyAttacked);
         //ReactFrom
         _stateMachine.AddTransition(_reactFromFrontS, patrolS, () => !_playerInSightRange && !_playerInAttackRange);
-        _stateMachine.AddTransition(_reactFromFrontS, chaseS, () => _playerInSightRange && !_playerInAttackRange);
-        _stateMachine.AddTransition(_reactFromFrontS, wonderS, () => _playerInSightRange && _playerInAttackRange);
-
-
+        _stateMachine.AddTransition(_reactFromFrontS, chaseS, () => !_graceActive && _playerInSightRange && !_playerInAttackRange);
+        _stateMachine.AddTransition(_reactFromFrontS, wonderS, () => !_graceActive && _playerInSightRange && _playerInAttackRange);
+        
         //Set Initial state
         _stateMachine.SetState(idleS);
     }
 
     void Update()
     {
-        //Check for sight and attack range
-        _playerInSightRange = Physics.CheckSphere(transform.position, _sightRange, _whatIsPlayer);
-        _playerInAttackRange = Physics.CheckSphere(transform.position, _attackRange, _whatIsPlayer);
-        if(_playerInAttackRange) {
-            if(Random.Range(0, 2) == 0) {
-                shortDistanceSpit = false;
+        // Grace period management
+        if (_graceActive)
+        {
+            _graceTimer -= Time.deltaTime;
+            if (_graceTimer <= 0f)
+            {
+                _graceActive = false;
             }
-            else {
-				shortDistanceSpit = true;
-			}
         }
 
-        _stateMachine.Tik();
+        // If we are in the grace period, then we set the flags to false
+        if (_graceActive)
+        {
+            _playerInSightRange = false;
+            _playerInAttackRange = false;
+        }
+
+        else
+        {
+            //Check for sight and attack range
+            _playerInSightRange = Physics.CheckSphere(transform.position, _sightRange, _whatIsPlayer);
+            _playerInAttackRange = Physics.CheckSphere(transform.position, _attackRange, _whatIsPlayer);
+            if(_playerInAttackRange) {
+                if(Random.Range(0, 2) == 0) {
+                    shortDistanceSpit = false;
+                }
+                else {
+                    shortDistanceSpit = true;
+                }
+            }
+
+            _stateMachine.Tik();   
+        }
     }
     public void Initialize(EnemyData enemyData, RoomManager.RoomManager roomManager)
     {
+        // Resets grace period at each spawn
+        _graceTimer = _gracePeriod;
+        _graceActive = true;
+        
         _roomManager = roomManager;
 
         if (!_agent) _agent = GetComponent<NavMeshAgent>();
