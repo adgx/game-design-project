1753fbc4d3e7cb3149106b77708a7058e8e55f5b
adgx <antonino.d.gregorio@gmail.com>
2025-08-31 16:58:17 +0200

    small glow for the interctable gameobject

--- GD-unity-project/Assets/Scripts/Utils/PulseEmission.cs
+++ GD-unity-project/Assets/Scripts/Utils/PulseEmission.cs
@@ -10,6 +10,7 @@ public class PulseEmission : MonoBehaviour
     [SerializeField]
     [Min(0)]
     private float flickerSpeed = 1.0f;
+    private float intensityRate = 1.5f; 
 
     [SerializeField] private AnimationCurve brightnessCurve;
 
@@ -44,16 +45,20 @@ public class PulseEmission : MonoBehaviour
         if(renderer.isVisible && Physics.CheckSphere(transform.position, 6f, whatIsPlayer) && !Physics.CheckSphere(transform.position, 2f, whatIsPlayer)) {
             float scaledTime = Time.time * flickerSpeed;
 
-            for(int i = 0; i < materials.Count; i++) {
+            for (int i = 0; i < materials.Count; i++)
+            {
 
                 float brightness = brightnessCurve.Evaluate(scaledTime);
-
-                materials[i].SetColor(EMISSIVE_COLOR_NAME, initialColors[i] * brightness);
+                float intensityHDR = Mathf.Pow(2, brightness * intensityRate);
+                materials[i].SetColor(EMISSIVE_COLOR_NAME, Color.white * intensityHDR);
+                materials[i].EnableKeyword(EMISSIVE_KEYWORD);
             }
         }
         else {
-			for(int i = 0; i < materials.Count; i++) {
-				materials[i].SetColor(EMISSIVE_COLOR_NAME, initialColors[i] * 0);
+            for (int i = 0; i < materials.Count; i++)
+            {
+                materials[i].SetColor(EMISSIVE_COLOR_NAME, Color.white * 0);
+                materials[i].EnableKeyword(EMISSIVE_KEYWORD);
 			}
 		}
     }
